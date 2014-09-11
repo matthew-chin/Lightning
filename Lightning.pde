579ed46ded33d4e9c6fc98aa9dee036f95e738bf
unknown <machin@697-334-Thorin.sf.k12.internal>
2014-09-11 11:19:06 -0700

    Lightning 1st time

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,67 @@
+bolt[] zap = new bolt[10];
+
+
 void setup()
 {
-  size(300,300);
+	size(300,300);
+	background(0);
+	noLoop();
 }
+
 void draw()
 {
-
+	for(int i = 0; i < 10; i++)
+	{
+		zap[i] = new bolt();
+		zap[i].show();
+	}
 }
+
 void mousePressed()
 {
-
+	background(0);
+	redraw();
 }
 
+class bolt
+{
+	int x, y, x2, y2;
+	bolt()
+	{
+	}
+	void show()
+	{
+		stroke(255);
+		
+		x = mouseX;
+		x2 = mouseX;
+		y = mouseY;
+		y2 = mouseY;
+		while(x < 300)
+		{
+			stroke((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+			
+			int plusX, plusY;
+			plusX = (int)(Math.random()*10) + 5;
+			plusY = (int)(Math.random()*20) -10;
+			
+			line(x, y, x+ plusX, y+ plusY);
+			
+			x += plusX;
+			y += plusY;
+		}
+		while(x2 > 0)
+		{
+			stroke((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+			
+			int plusX2, plusY2;
+			plusX2 = (int)(Math.random()*10) - 15;
+			plusY2 = (int)(Math.random()*20) -10;
+			
+			line(x2, y2, x2 + plusX2, y2 + plusY2);
+			
+			x2 += plusX2;
+			y2 += plusY2;
+		}
+	}
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,73 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+bolt[] zap = new bolt[10];
+
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
-}
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
-}
-public void mousePressed()
-{
-  redraw();
+	size(300,300);
+	background(0);
+	noLoop();
 }
 
+public void draw()
+{
+	for(int i = 0; i < 10; i++)
+	{
+		zap[i] = new bolt();
+		zap[i].show();
+	}
+}
+
+public void mousePressed()
+{
+	background(0);
+	redraw();
+}
+
+class bolt
+{
+	int x, y, x2, y2;
+	bolt()
+	{
+	}
+	public void show()
+	{
+		stroke(255);
+		
+		x = mouseX;
+		x2 = mouseX;
+		y = mouseY;
+		y2 = mouseY;
+		while(x < 300)
+		{
+			stroke((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+			
+			int plusX, plusY;
+			plusX = (int)(Math.random()*10) + 5;
+			plusY = (int)(Math.random()*20) -10;
+			
+			line(x, y, x+ plusX, y+ plusY);
+			
+			x += plusX;
+			y += plusY;
+		}
+		while(x2 > 0)
+		{
+			stroke((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+			
+			int plusX2, plusY2;
+			plusX2 = (int)(Math.random()*10) - 15;
+			plusY2 = (int)(Math.random()*20) -10;
+			
+			line(x2, y2, x2 + plusX2, y2 + plusY2);
+			
+			x2 += plusX2;
+			y2 += plusY2;
+		}
+	}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
